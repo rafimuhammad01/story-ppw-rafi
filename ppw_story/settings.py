402c74fb1de2eebcfacd069925f6543261182b1b
rafimuhammad01 <rafi10muhammad@gmail.com>
2020-11-24 19:45:29 +0700

    [REFACTOR] add navbar and footer

--- ppw_story/settings.py
+++ ppw_story/settings.py
@@ -143,5 +143,8 @@ STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
