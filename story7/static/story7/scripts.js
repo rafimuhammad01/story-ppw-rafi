1ee3807d5d0fd40434984797fbb2ce3b0b94caab
rafimuhammad01 <rafi10muhammad@gmail.com>
2020-11-24 23:21:59 +0700

    [REFACTOR] add style to accordion

--- story7/static/story7/scripts.js
+++ story7/static/story7/scripts.js
@@ -1,7 +1,25 @@
-$(document).ready(function(){
+$( function() {
+    $( "#accordion" )
+      .accordion({
+        header: "> div > h3 ",
+        heightStyle: "content",
+        collapsible: true ,
+        clearStyle: true, 
+        autoHeight: false,
+        active: false,
+        
+      })
+      .sortable({
+        axis: "y",
+        handle: ".arrow",
+        stop: function( event, ui ) {
+          // IE doesn't register the blur when sorting
+          // so trigger focusout handlers to remove .ui-state-focus
+          ui.item.children( ".arrow" ).triggerHandler( "focusout" );
+ 
+          // Refresh accordion to handle new order
+          $( this ).accordion( "refresh" );
+        }
 
-    $( function() {
-        $( "#accordion" ).accordion();
-      } );
-  
-});
+      });
+  } );
