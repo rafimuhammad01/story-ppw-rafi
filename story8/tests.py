42847c26433736e470466cba7d550d67834b809e
rafimuhammad01 <rafi10muhammad@gmail.com>
2020-12-02 19:13:34 +0700

    [GREEN] Passed url testing

--- story8/tests.py
+++ story8/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 
 # Create your tests here.
 
---
+++ story8/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'story8'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('data', views.data, name='data')
+]
