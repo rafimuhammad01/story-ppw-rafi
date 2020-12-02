ca845df64bc3fdbcb38e8170bf3de5dbeb9469fa
rafimuhammad01 <rafi10muhammad@gmail.com>
2020-12-02 19:19:13 +0700

    [RED] Adding template

--- story8/tests.py
+++ story8/tests.py
@@ -6,7 +6,10 @@ class MainTestCase(TestCase):
     def test_url(self) :
         response = Client().get("/story8/")
         self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,"story8/index.html")
 
     def test_url_data(self) :
         response = Client().get("/story8/data")
         self.assertEqual(response.status_code,200)
+
+    
