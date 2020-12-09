e7d184668e7238cc1d8801368a420060956da6e1
rafimuhammad01 <rafi10muhammad@gmail.com>
2020-12-09 17:13:43 +0700

    [RED] login feature

--- story9/tests.py
+++ story9/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase,Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class MainTestCase(TestCase):
@@ -10,6 +11,15 @@ class MainTestCase(TestCase):
         response = Client().get("/story9/login")
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response,"story9/login.html")
+
+    def test_login(self) :
+        user = User.objects.create_user('testing', 'testing@testing.com', 'testing8888')
+        response = self.client.post("/story9/login", data={'username' : 'testing', 'password' : 'testing888'})
+        self.assertEqual(response.status_code,302)
+
+        response = Client().get("/story9/")
+        html_response = response.content.decode('utf8')
+        self.assertIn("testing", html_response)
         
     
        
