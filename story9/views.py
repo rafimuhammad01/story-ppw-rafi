0badd3bb125be5212c27a5269f63b82f97c99c80
rafimuhammad01 <rafi10muhammad@gmail.com>
2020-12-09 17:03:21 +0700

    [GREEN] pass url and template test

--- story9/views.py
+++ story9/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request) :
+    return render(request, 'story9/index.html')
+
+def login(request) :
+    return render(request, 'story9/login.html')
